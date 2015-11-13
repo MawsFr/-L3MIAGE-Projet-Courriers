27fe051eeb0bbe2a81b604d678a5c84ac30fd93d
Maws <mnezzari@gmail.com>
2015-11-13 09:36:07 +0100

    Changement de la déclaration de SimpleLetter pour faire passer une string au lieu d'un content
    Implémentation de la classe Text
    Ajout de la fonction getContent et d'un constructeur

--- Projet 3 Courriers/src/model/SimpleLetter.java
+++ Projet 3 Courriers/src/model/SimpleLetter.java
@@ -2,8 +2,8 @@ package model;
 
 public class SimpleLetter extends Letter<Text> {
 
-	public SimpleLetter(Inhabitant sender, Inhabitant receiver, Content content, double cost) {
-		super(sender, receiver, content, 1);
+	public SimpleLetter(Inhabitant sender, Inhabitant receiver, String content, double cost) {
+		super(sender, receiver, new Text(content), 1);
 	}
 	
 	@Override
--- Projet 3 Courriers/src/model/Text.java
+++ Projet 3 Courriers/src/model/Text.java
@@ -1,5 +1,16 @@
 package model;
 
 public class Text implements Content {
+	
+	protected String content;
+	
+	public Text(String content) {
+		this.content = content;
+	}
+	
+	@Override
+	public String getContent() {
+		return this.content;
+	}
 
 }
