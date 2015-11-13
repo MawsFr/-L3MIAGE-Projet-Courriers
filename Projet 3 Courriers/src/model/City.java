c8c370d2681098515e88e75a82b0b0a9c6680dd8
Togam <margot_six@hotmail.com>
2015-11-13 09:10:44 +0100

    Création des classes City et Inhabitant (vides pour le moment)

---
+++ Projet 3 Courriers/src/model/City.java
@@ -0,0 +1,17 @@
+package model;
+
+import java.util.List;
+
+public class City {
+
+	protected String name;
+	protected List<Letter> postBox;
+	
+	public void sendLetter(Letter<?> letter){
+		
+	}
+	
+	public void distibuteLetters(){
+		
+	}
+}
---
+++ Projet 3 Courriers/src/model/Inhabitant.java
@@ -0,0 +1,24 @@
+package model;
+
+public class Inhabitant {
+
+	protected String name;
+	protected City city;
+	protected int bankAccount;
+	
+	public void credit(int money){
+		
+	}
+	
+	public void debit(int money){
+		
+	}
+	
+	public void sentdLetter(Letter<?> letter){
+		
+	}
+	
+	public void receiveLetter(Letter<?> letter){
+		
+	}
+}
